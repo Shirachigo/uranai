de8fcccee86e74a3ac193a8856cc21f71e08e5d8
Shirachigo <fue.oshi@gmail.com>
2023-10-18 00:23:52 +0900

    app.py修正

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def soulno_01():
 def compatibility_six_stars():
     return render_template('compatibility_six_stars.html')
 
-app.run(port=5000, debug=True)
+app.run(host='133.242.185.142', port=5000, debug=True)
