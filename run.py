b634f1ec6bfc29572953b0a0c49fa085fdf65473
Shirachigo <fue.oshi@gmail.com>
2023-10-18 00:14:05 +0900

    run.py追加

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__) 
+
+@app.route('/')
+def hello():
+    text = "Hello World!"
+    return jsonify(message = text)
+
+if __name__ == '__main__':
+    app.run()
